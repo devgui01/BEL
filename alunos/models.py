53063809fe9230ed95e43ff56db92b612e627661
Guilherme Kauan <123951833+devgui01@users.noreply.github.com>
2025-08-14 14:44:26 -0400

    SOLUÇÃO DEFINITIVA: Removido Pillow problemático e campo avatar temporariamente

--- alunos/models.py
+++ alunos/models.py
@@ -68,7 +68,7 @@ class Pagamento(models.Model):
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+    # avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)  # Temporariamente comentado
     nome_completo = models.CharField(max_length=100, blank=True)
     telefone = models.CharField(max_length=20, blank=True)
     endereco = models.CharField(max_length=200, blank=True)
